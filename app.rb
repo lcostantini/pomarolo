5ba16f291dc86847271ea6c7dd2bbd44c2ef9e46
lcostantini <costantinileandro1@gmail.com>
2014-09-11 14:45:51 -0300

    Changes to adding render for add pomarolos

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require "cuba"
 require "mote"
+require "mote/render"
 require "ohm"
 
+
+Cuba.plugin Mote::Render
 Cuba.plugin Mote::Helpers
 
 Dir["./models/**/*.rb"].each { |rb| require rb }
@@ -9,29 +12,15 @@ Dir["./models/**/*.rb"].each { |rb| require rb }
 Cuba.define do
   
   on root do
+    edit = Pomodoro.new({})
+    render("add_pomodoro", edit: edit, title: "Pomarolos")
+    on param("pomodoro") do |params|
+      pomodoro = Pomodoro.create(params)
+      res.redirect "/"
+    end
     res.write mote("views/layout.mote",
       title: "Pomarolos",
       content: mote("views/home.mote", pomodoros: Pomodoro.all ))
-  end
-  
-  on "add_pomodoro" do
-    on param("pomodoro") do |params|
-      pomodoro = Pomodoro.create(params)
-      res.write mote("views/layout.mote",
-        title: "New pomarolo added",
-        content: mote("views/done.mote",
-          restaurant_id: "",
-          message: "New pomarolo added.",
-          url: "/",
-          button_message: "Back to pomarolos list"))
-    end
-    on default do
-      edit = Pomodoro.new({})
-      res.write mote("views/layout.mote",
-        title: "Add pomarolo",
-        content: mote("views/add_pomodoro.mote",
-          edit: edit))
-    end
   end
   
   on "pomarolo/:pomodoro_id" do |pomodoro_id|
@@ -42,9 +31,7 @@ Cuba.define do
       pomodoro.finish = true
     end
     pomodoro.save
-    res.write mote("views/layout.mote",
-      title: "Pomarolos",
-      content: mote("views/home.mote", pomodoros: Pomodoro.all ))
+    res.redirect "/"
   end
   
 end
