797170d0b07e0918cdecd545d424d997deb59d08
lcostantini <costantinileandro1@gmail.com>
2014-09-15 13:28:40 -0300

    Changes in app

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "cuba"
 require "mote"
 require "mote/render"
 require "ohm"
+require_relative "./lib/authentication/authentication"
 
 Cuba.plugin Mote::Render
 Cuba.plugin Mote::Helpers
@@ -9,22 +10,36 @@ Cuba.plugin Mote::Helpers
 Dir["./models/**/*.rb"].each { |rb| require rb }
 
 Cuba.define do
-  
   on root do
+    run Authentication
+  end
+  
+  on "logout" do
+    run Authentication
+  end
+  
+  on "signup" do
+    run Authentication
+  end
+  
+  on "login" do
+    run Authentication
+  end
+  
+  on "user" do
     edit = Pomodoro.new({})
     render("add_pomodoro", edit: edit, title: "Pomarolos")
     on param("pomodoro") do |params|
       pomodoro = Pomodoro.create(params)
-      res.redirect "/"
+      res.redirect "/user"
     end
     res.write mote("views/layout.mote",
       title: "Pomarolos",
       content: mote("views/home.mote", pomodoros: Pomodoro.all ))
   end
-  
+
   on "pomarolo/:pomodoro_id" do |pomodoro_id|
     pomodoro = Pomodoro[pomodoro_id].swap_finish
-    res.redirect "/"
-  end
-  
+    res.redirect "/user"
+  end 
 end
