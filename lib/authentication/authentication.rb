c3fd44dd337ed059f83a9f1dede106753f4b3bba
lcostantini <costantinileandro1@gmail.com>
2014-10-07 10:24:23 -0300

    Clean code in Authentication app

--- lib/authentication/authentication.rb
+++ lib/authentication/authentication.rb
@@ -19,25 +19,20 @@ class Authentication < Cuba
   define do
     on root do
       res.write mote("lib/authentication/views/layout.mote",
-        title: "Authentication",
         user_id: session[:user],
         content: mote("lib/authentication/views/home.mote"))
     end
     
     on "signup" do
+      res.write mote("lib/authentication/views/signup.mote", user: User.new)
       on param("user") do |params|
-        u = User.create(params)
+        User.create(params)
         res.redirect ("/user")
-      end
-      
-      on default do
-        edit = User.new({})
-        res.write mote("lib/authentication/views/layout.mote", title: "Sign up",
-          content: mote("lib/authentication/views/signup.mote", edit: edit))
       end
     end
     
     on "login" do
+      res.write mote("lib/authentication/views/login.mote", user: User.new)
       on param("user") do |params|
         user = User.with(:email, params["email"]) unless nil
         if user && user.password == params["password"]
@@ -45,25 +40,15 @@ class Authentication < Cuba
           res.redirect ("/user")
         else
           res.write mote("lib/authentication/views/layout.mote",
-            title: "Login",
             message: "Email or password are incorrect.",
             content: mote("lib/authentication/views/login.mote", params: params))
         end
-      end
-      
-      on default do
-        edit = User.new({})
-        res.write mote("lib/authentication/views/layout.mote", title: "Login",
-          content: mote("lib/authentication/views/login.mote", edit: edit))
       end
     end
 
     on "logout" do
       session.delete(:user)
-      res.write mote("lib/authentication/views/layout.mote",
-        title: "Authentication",
-        user_id: session[:user],
-        content: mote("lib/authentication/views/home.mote"))
+      res.redirect ("/user")
     end
   end
 end
